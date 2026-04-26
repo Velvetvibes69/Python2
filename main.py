92f408aef97ce17d41986b1afa9019803e874b37
Velvetvibes69 <amayasunil11@gmail.com>
2026-04-26 22:10:18 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print("My Daily Routine")
+
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8 AM.")
+
+# Step 3: Watch TV
+print("I watch TV in the evening.")
+
+# Step 4: Studying
+print("I study for 1 hour at night.")
+
+print("Total study hours in a week:", 2 * 7)
